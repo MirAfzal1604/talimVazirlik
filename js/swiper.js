d5668b76209a3e9b24c5e52ceb0929e07723904e
MirAfzal <92784299+MirAfzal1604@users.noreply.github.com>
2022-02-15 21:28:22 +0500

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -36,3 +36,40 @@ var swiper = new Swiper(".feedBackSwiper", {
     grabCursor: true,
     loop: true,
 });
+
+
+var swiper = new Swiper(".footerlinks", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // 200: {
+        //     slidesPerView: 1,
+        //     spaceBetween: 0,
+        // },
+        // 400: {
+        //     slidesPerView: 3,
+        //     spaceBetween: 5,
+        // },
+        569: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+        },
+        1600: {
+            slidesPerView: 6,
+            spaceBetween: 15,
+        }
+    },
+});
